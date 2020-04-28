806b6fbc5d77c9aa180e707de5dbd27c66fe0510
Roman Matveev <r.matveev@cek.ru>
2020-04-28 15:56:56 +0300

    Simple project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node video stream
+
+Video Stream With Node.js and HTML5
+
+## Installation
+
+* `git clone`
+* `cd node-video-stream`
+* `npm install`
+* `npm start`
+* visit http://localhost:3000/
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.htm'));
+})
+
+app.get('/stream', function(req, res) {
+  const path = 'assets/sample.mp4';
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = +parts[0];
+    const end = parts[1]
+      ? +parts[1]
+      : fileSize-1;
+
+    if(start >= fileSize) {
+      res.status(416).send('Запрошенный диапазон невыполним\n'+start+' >= '+fileSize);
+      return;
+    }
+    
+    const chunkSize = (end-start)+1;
+    const file = fs.createReadStream(path, {start, end});
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+})
+
+app.listen(3000, function () {
+  console.log(`Server running on ${PORT} port...`);
+});
